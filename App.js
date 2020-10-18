da25cc955de7c56c968802b933cf7407b9f17b31
mikeqiu2011 <mikeqiu2011@163.com>
2020-10-18 20:09:05 +0800

    starting at sytling

--- App.js
+++ App.js
@@ -2,10 +2,26 @@ import React from "react";
 
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import { View } from "react-native";
 
 export default function App() {
-  // return <WelcomeScreen />;
-  return <ViewImageScreen />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: 100,
+          height: 100,
+        }}
+      ></View>
+    </View>
+  );
 }
 
 //rnss - react native style sheet

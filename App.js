99bbd19aa35423937b858951699c9ceff6a33b67
mikeqiu2011 <mikeqiu2011@163.com>
2020-10-18 20:26:37 +0800

    padding

--- App.js
+++ App.js
@@ -18,10 +18,24 @@ export default function App() {
           backgroundColor: "dodgerblue",
           width: 100,
           height: 100,
-          shadowColor: "grey",
-          shadowOffset: { width: 10, height: 10 },
-          shadowOpacity: 0.5, // 0 is no shadow, 1 is full shadow
-          shadowRadius: 10,
+          padding: 20,
+          paddingHorizontal: 10,
+          paddingLeft: 30,
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "gold",
+            width: 50,
+            height: 50,
+          }}
+        ></View>
+      </View>
+      <View
+        style={{
+          backgroundColor: "tomato",
+          width: 100,
+          height: 100,
         }}
       ></View>
     </View>

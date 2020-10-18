e88d701d5c6392107b04cb02fd59033b971cc196
mikeqiu2011 <mikeqiu2011@163.com>
2020-10-18 16:43:16 +0800

    align logo to center

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -21,6 +21,7 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
+    alignItems: "center",
   },
   loginButton: {
     backgroundColor: "yellow",

9d84c4423da7e8582e9bf56550ac800a69514740
mikeqiu2011 <mikeqiu2011@163.com>
2020-10-18 16:37:16 +0800

    added bgImage and button and align to bottom

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,55 +1,35 @@
 import React from "react";
-import { StyleSheet, View, Image, ImageBackground } from "react-native";
+import { StyleSheet, View, Image, ImageBackground, Button } from "react-native";
 
 function WelcomeScreen(props) {
   return (
-    <View style={styles.container}>
-      <View
+    <ImageBackground
+      source={require("../assets/background.jpg")}
+      style={styles.background}
+    >
+      {/* <Image
+        source={require("../assets/logo-red.png")}
         style={{
-          backgroundColor: "gold",
-          flex: 8,
+          width: 200,
+          height: 200,
+          justifyContent: "center",
+          alignItems: "center",
         }}
-      >
-        <ImageBackground
-          source={require("../assets/background.jpg")}
-          style={{
-            width: "100%",
-            height: "100%",
-          }}
-        >
-          <Image
-            source={require("../assets/logo-red.png")}
-            style={{
-              width: 200,
-              height: 200,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          ></Image>
-        </ImageBackground>
-      </View>
-      <View
-        style={{
-          backgroundColor: "orange",
-          flex: 1,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: "dodgerblue",
-          flex: 1,
-        }}
-      />
-    </View>
+      ></Image> */}
+      <View style={styles.loginButton} />
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
+    justifyContent: "flex-end",
+  },
+  loginButton: {
+    backgroundColor: "yellow",
+    width: "100%",
+    height: 70,
   },
 });
 

7492d699e3fb82a1e4de84f837f6c8772ab788bb
mikeqiu2011 <mikeqiu2011@163.com>
2020-10-18 20:38:30 +0800

    styling text

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -13,32 +13,22 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <View
+      <Text
         style={{
-          backgroundColor: "dodgerblue",
-          width: 100,
-          height: 100,
-          padding: 20,
-          paddingHorizontal: 10,
-          paddingLeft: 30,
+          fontFamily: "Courier", //this is IOS only
+          fontSize: 30,
+          fontStyle: "italic",
+          fontWeight: "600",
+          color: "tomato",
+          textTransform: "uppercase",
+          textDecorationLine: "line-through",
+          textAlign: "justify",
+          lineHeight: 30,
         }}
       >
-        <View
-          style={{
-            backgroundColor: "gold",
-            width: 50,
-            height: 50,
-          }}
-        ></View>
-      </View>
-      <View
-        style={{
-          backgroundColor: "tomato",
-          width: 100,
-          height: 100,
-          margin: 20,
-        }}
-      ></View>
+        I love React Native, this is my best course, very hands on, very
+        interesting, bite sized, very easy to master
+      </Text>
     </View>
   );
 }
